78bda5e0623a60127b2ec9c7a90e4c3d515dce97
MoonJongHyeon1095 <109948801+MoonJongHyeon1095@users.noreply.github.com>
2023-09-22 03:34:11 +0900

    fix : 연결문제로 인한 CorConfig 주석처리

--- src/main/java/com/github/chatserver/dto/ChatRmqDto.java
+++ src/main/java/com/github/chatserver/dto/ChatRmqDto.java
@@ -11,7 +11,6 @@ import java.util.Map;
 @Builder
 public class ChatRmqDto {
     private String customRoomId;
-    private Integer messageTag;
     private String sender;
     private String content;
     private String createdAt;
